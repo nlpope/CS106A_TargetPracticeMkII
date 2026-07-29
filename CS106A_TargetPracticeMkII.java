77d9748aebbf6feb676660d08800bab9909f2186
Noah Pope <npope@tutanota.com>
2026-07-29 09:48:12 -0500

    adding files to new repo

---
+++ CS106A_TargetPracticeMkII.java
@@ -0,0 +1,54 @@
+/**
+ * File: CS106A_TargetPracticeMkII.java
+ * ----------------------------
+ * This Java program draws a target to the canvas using more
+ * streamlined center variables (cx,cy) and methods 
+ * (createFilledCircle) than its predecessor.
+ * 
+ */
+
+import acm.program.*;
+import acm.graphics.*;
+import java.awt.*;
+
+public class CS106A_TargetPracticeMkII extends GraphicsProgram
+{	
+	public void run()
+	{ 
+		createTarget();
+	}
+	
+	
+	private void createTarget()
+	{	
+		//center x & y
+		double cx = getWidth() / 2;
+		double cy = getHeight() / 2;
+		
+		for(int i = 0; i < 5; i++){
+			//add an x, add a y every time 
+			double x = cx;
+			double y = cy;
+			double r = OUTER_RADIUS - (i * INNER_RADIUS + 20);
+			Color circleColor = i % 2 == 0 ? Color.PINK : Color.WHITE;
+			GOval newCircle = createFilledCircle(x, y, r, circleColor);
+			add(newCircle);
+		}
+		
+	}
+	
+	
+	private GOval createFilledCircle(
+			double x, double y,
+			double r, Color color)
+	{
+		GOval circle = new GOval(x-r, y-r, 2*r, 2*r);
+		circle.setColor(color);
+		circle.setFilled(true);
+		return circle;
+	}
+	
+	private static final int N_CIRCLES = 5;
+	private static final double OUTER_RADIUS = 75;
+	private static final double INNER_RADIUS = 10;
+}
